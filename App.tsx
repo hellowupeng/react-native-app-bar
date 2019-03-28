a2b57d9d6204e1c21d1a0c4e857092989265fd39
andywu <hellowupeng@gmail.com>
2019-03-28 21:00:47 +0800

    init

---
+++ App.tsx
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import Example from './example';
+
+export default class App extends Component {
+  render() {
+    return <Example />;
+  }
+}
